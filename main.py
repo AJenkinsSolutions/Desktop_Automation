c4287f1ff1b65fb7c29d6fea3c47dbd97d5c204c
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-06-06 11:18:30 -0500

    Add Audio fucntion

--- main.py
+++ main.py
@@ -12,15 +12,13 @@ audio = (".3ga", ".aac", ".ac3", ".aif", ".aiff",
          ".mp3", ".mpga", ".ogg", ".oga", ".mogg",
          ".opus", ".qcp", ".tta", ".voc", ".wav",
          ".wma", ".wv")
-
-
 video = (".webm", ".MTS", ".M2TS", ".TS", ".mov",
          ".mp4", ".m4p", ".m4v", ".mxf", ".MOV")
-
 img = (".jpg", ".jpeg", ".jfif", ".pjpeg", ".pjp", ".png",
        ".gif", ".webp", ".svg", ".apng", ".avif", ".JPG")
 
-
+#TODO 1, Automaticly create images, Audio, screens and video folders in users documents. not if already exisits
+# Use crontab.guru to set up automation of this task every sunday
 
 def is_video(file):
     """
@@ -29,14 +27,14 @@ def is_video(file):
     if extention is in our list of video file extentions it returns
     """
     return os.path.splitext(file)[1] in video
-
 def is_screenshot(file):
     name, ext = os.path.splitext(file)
     #If screen shot is specified within the name
     return (ext in img) and "screenshot" or "screen shot" in name.lower()
-
 def is_image(file):
     return os.path.splitext(file)[1] in img
+def is_audio(file):
+    return os.path.splitext(file)[1] in audio
 
 #   Change working Directory to desktop
 os.chdir("/Users/apjenkins/Desktop")
@@ -47,8 +45,12 @@ os.chdir("/Users/apjenkins/Desktop")
 for file in os.listdir():
     if is_video(file):
         shutil.move(file, "/Users/apjenkins/Documents/Videos")
+    elif is_audio(file):
+        shutil.move(file, "/Users/apjenkins/Documents/Audio")
     elif is_image(file):
         if is_screenshot(file):
             shutil.move(file, "/Users/apjenkins/Documents/ScreenShots")
         else:
             shutil.move(file, "/Users/apjenkins/Documents/Images")
+    else:
+        shutil.move(file, "/Users/apjenkins/Documents")
